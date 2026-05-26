ccefc81243b2a69c0d28116ea61de3318821da02
Levente Kiss <levente.kiss@solarpunk.buzz>
2026-05-26 15:06:26 +0200

    refactor: address PR review - extract ConfirmationModal default text constants and split Footer into focused files

---
+++ src/components/Footer/CalendarIcon.tsx
@@ -0,0 +1,15 @@
+export function CalendarIcon() {
+  return (
+    <svg className="footer-calendar-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 16">
+      <g fill="#F6F7F9" clipPath="url(#footer-cal-a)">
+        <path d="M8.467 14h-6V6h12v2h2V4a1 1 0 0 0-1-1h-2V1a1 1 0 0 0-2 0v2h-6V1a1 1 0 0 0-2 0v2h-2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h7v-2Z" />
+        <path d="M15.967 12h-1.5v-1.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5V12h-1.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1.5v1.5a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V14h1.5a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5Z" />
+      </g>
+      <defs>
+        <clipPath id="footer-cal-a">
+          <path fill="#fff" d="M.467 0h16v16h-16z" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
